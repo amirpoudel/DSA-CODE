fa35ae02750d5d668019ecbf9f75223beab1c137
Amir Poudel <amirpoudel2058@gmail.com>
2023-02-24 00:34:56 +0545

    print LCS

--- Dynamic Programming/LCS/printLCSubsequence.cpp
+++ Dynamic Programming/LCS/printLCSubsequence.cpp
@@ -7,38 +7,47 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-void solve(string &str1,string &str2,int x,int y,string &lcs){
-    if(x<0 or y<0){
 
-        return ;
+
+string solve(string &str1,string &str2,int x,int y){
+    if(x< 0 or y<0){
+        return "";
     }
 
     if(str1[x]==str2[y]){
-        lcs.push_back(str1[x]);
-        cout<<"Push back "<<str1[x]<<"  "<<str2[y]<<endl;
-        solve(str1,str2,x-1,y-1,lcs);
+        return str1[x]+ solve(str1,str2,x-1,y-1);
     }else{
-
-        solve(str1,str2,x,y-1,lcs);
-        solve(str1,str2,x-1,y,lcs);
+        string lcs1 = solve(str1,str2,x,y-1);
+        string lcs2 = solve(str1,str2,x-1,y);
+        return lcs1.size()> lcs2.size()?lcs1:lcs2;
     }
+}
+
+string solveTab(string &str1,string &str2){
+
+
+
+
 
 }
-string printLCS(string &str1,string &str2){
 
-    string lcs;
+void printLCS(string &str1,string &str2 ){
     int x = str1.size()-1;
     int y = str2.size()-1;
-    solve(str1,str2,x,y,lcs);
-    return lcs;
 
+    string ans = solve(str1,str2,x,y);
+    reverse(ans.begin(),ans.end());
+    cout<<ans;
 }
+
 
 int main(){
 
     string  str1 = "abcde";
     string str2 =  "abfce";
-    cout<<printLCS(str1,str2);
+    vector<char> ans;
+
+    printLCS(str1,str2);
 
     return 1;
 }
